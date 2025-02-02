e9d091108df3e26d4e429c5ac8167a644ef89b8a
FlatEarthRocCo <ferc.rockettechnologies@gmail.com>
2025-02-02 10:56:54 +0100

    Enemy movement added

--- Astro survival/Assets/Scripts/Enemy.cs
+++ Astro survival/Assets/Scripts/Enemy.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    public float range;
+    private Vector3 direction;
+
     [SerializeField]
     private int damage = 5;
     [SerializeField]
@@ -21,11 +24,43 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Swarm();
+        Vector3 Offset = transform.position - player.transform.position;
+        float Distance = Offset.magnitude;
+        if(Distance <= range) {
+            Swarm();
+        }
+
     }
 
     private void Swarm()
     {
+        if (player == null)
+            return;
+
+        Vector3 targetPosition = player.transform.position;
+        direction = targetPosition - transform.position;
+
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            direction.y = 0;
+            if (direction.x > 0)
+                direction.x = 1;
+            else
+                direction.x = -1;
+        }
+        else
+        {
+            direction.x = 0;
+            if (direction.y > 0)
+                direction.y = 1;
+            else
+                direction.y = -1;
+        }
+
+        direction.Normalize();
+
+        transform.position += direction * speed * Time.deltaTime;
+        
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);    
     }
 
