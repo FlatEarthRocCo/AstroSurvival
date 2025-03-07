88756c3a9c8d736498efc1dc75dee8e116db7376
FlatEarthRocCo <ferc.rockettechnologies@gmail.com>
2025-03-07 14:16:06 +0100

    Player's laser is facing right direction, ready to implement destroying enemy function.

--- Astro survival/Assets/Scripts/Laser.cs
+++ Astro survival/Assets/Scripts/Laser.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 dir = transform.GetComponent<Rigidbody2D>().velocity;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
